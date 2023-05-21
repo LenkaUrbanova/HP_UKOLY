6e1b489c6a70e1f56c708ce48bbf6137674859c3
Lenka Urbanová <lenkahevierova@gmail.com>
2023-05-21 22:31:00 +0200

    UKOL7F

--- Ukol7f.py
+++ Ukol7f.py
@@ -7,10 +7,10 @@ class Auto():    # definice konkretni nove tridy
 
     def pujc_auto(self):
         if self.dostupne:
-            self.dostupne = True
-            return "Potvrzuji zapůjčení vozidla"
+            self.dostupne = False
+            return "Vozidlo je dostupné"
         else:
-            return "Vozidlo není k dispozici"
+            return "Vozidlo není dostupné"
 
     def get_info(self):
         return f"Auto {self.typ_vozidla}, reg. značka {self.registracni_znacka}"
@@ -19,7 +19,7 @@ class Auto():    # definice konkretni nove tridy
 peugeot = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
 skoda = Auto("1P3 4747", "Škoda Octavia", 41253)
 
-zadany_typ = input("Jake auto si prejete pujcit? ")
+zadany_typ = input("Jaké vozidlo si přejete zapůjčit?")
 
 if zadany_typ == "Peugeot":
     print(peugeot.pujc_auto())
@@ -27,10 +27,10 @@ elif zadany_typ == "Škoda":
     print(skoda.pujc_auto())
 else:
     print(
-        f"Model '{zadany_typ}' není k dispozici. K dispozici jsou tyto modely: Peugeot, Škoda"
+        f"Vozidlo '{zadany_typ}' není k dispozici. K dispozici jsou pouze 2 modely: Peugeot, Škoda"
     )
 
-zadany_typ = input("Jake auto si prejete pujcit? ")
+zadany_typ = input("Jaké vozidlo si přejete zapůjčit?")
 
 if zadany_typ == "Peugeot":
     print(peugeot.pujc_auto())
@@ -38,5 +38,5 @@ elif zadany_typ == "Škoda":
     print(skoda.pujc_auto())
 else:
     print(
-        f"Model '{zadany_typ}' není k dispozici. K dispozici jsou tyto modely: Peugeot, Škoda"
+        f"Vozidlo '{zadany_typ}' není k dispozici. K dispozici jsou pouze 2 modely: Peugeot, Škoda"
     )
